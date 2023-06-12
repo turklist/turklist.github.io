29cf92766d79db9cb7cccb930ba064b076512751
gorgun <136341270+turklist@users.noreply.github.com>
2023-06-12 19:01:24 +0300

    Updatin
    yeah

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 Originally made by Electro. I am just using it as a template.
+
+			"vids": [
+				{
+					"user": "",
+					"link": "",
+					"percent": "",
+					"hz": ""
+				},	
+
+
+      ],
--- js/list.js
+++ js/list.js
@@ -6,7 +6,7 @@ const d = {
 				{
 					"user": "",
 					"link": "",
-					"percent": "",
+					"percent": "0",
 					"hz": ""
 				},	
 
@@ -22,6 +22,68 @@ const d = {
 			"key": 0
 		},
 		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "",
+					"link": "",
+					"percent": "0",
+					"hz": ""
+				},	
+
+
+      ],
+			"name": "Wasureta",
+			"author": "crazen [EvoPot]",
+			"more": "none",
+			"id": 64658786,
+			"pass": "Yok",
+			"percentToQualify": 58,
+			"verificationVid": "https://youtu.be/tiMG6i-qawE",
+			"key": 1
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "",
+					"link": "",
+					"percent": "0",
+					"hz": ""
+				},	
+
+
+      ],
+			"name": "Cybernetic Crescent",
+			"author": "Codex [Holojen]",
+			"more": "none",
+			"id": 66291197,
+			"pass": "010721",
+			"percentToQualify": 55,
+			"verificationVid": "https://youtu.be/DtDEiq8ep9Y",
+			"key": 2
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "",
+					"link": "",
+					"percent": "0",
+					"hz": ""
+				},	
+
+
+      ],
+			"name": "Sunset Sandstorm",
+			"author": "crohn44 [Yarkii]",
+			"more": "none",
+			"id": 596247,
+			"pass": "010721",
+			"percentToQualify": 39,
+			"verificationVid": "https://youtu.be/RqQTdOAMUuE",
+			"key": 3
+		},
 
 
 /*only modify version with every MAJOR update (you can always check update number at https://github.com/electroflameofficial/gdshittylist/releases*/
@@ -31,7 +93,7 @@ const d = {
 	"version": [
 		1,
 		0,
-		0
+		1
 	]
 };
 
