e7cd9b3d1006bd4c24c45e2691af588f7266660e
turklist <136341270+turklist@users.noreply.github.com>
2023-06-12 16:12:20 +0300

    Add files via upload

---
+++ js/list.js
@@ -0,0 +1,64 @@
+const d = {
+	"list": [
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "Waxyss",
+					"link": "www.youtube.com/watch?v=jpvBBeVg9Yk&ab_channel=Lacey",
+					"percent": "99",
+					"hz": "240Hz"
+				},	
+				{
+					"user": "gorgun",
+					"link": "www.youtube.com/watch?v=jpvBBeVg9Yk&ab_channel=Lacey",
+					"percent": "100",
+					"hz": "240Hz"
+				},	
+
+      ],
+			"name": "Waxyss Challenge",
+			"author": "Lacey [Lacey]",
+			"more": "none",
+			"id": 89653568,
+			"pass": "yok",
+			"percentToQualify": 57,
+			"verificationVid": "https://youtu.be/uRe5VE-91HI",
+			"key": 0
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "Lacey",
+					"link": "www.youtube.com/watch?v=jpvBBeVg9Yk&ab_channel=Lacey",
+					"percent": "99",
+					"hz": "360Hz"
+				},	
+
+      ],
+			"name": "Lacey Challenge",
+			"author": "gorgun [gorgun]",
+			"more": "none",
+			"id": 89653568,
+			"pass": "yok",
+			"percentToQualify": 57,
+			"verificationVid": "https://youtu.be/uRe5VE-91HI",
+			"key": 0
+		},
+
+
+
+
+/*only modify version with every MAJOR update (you can always check update number at https://github.com/electroflameofficial/gdshittylist/releases*/
+/*for legacy list add the word "legacy" behind the word key right away: "legacykey"*/
+
+	],
+	"version": [
+		1,
+		0,
+		0
+	]
+};
+
+const list = d.list;const version = d.version;
---
+++ readme.md
@@ -0,0 +1,41 @@
+# Welcome to the Official first release of the shitty list fellow stranger!
+---
+There isnt much going on here at the moment but trust me when i tell you you have *lots* of work you have to do, mainly in html and js, and maybe even in css if you wanna refresh the looks a bit.
+I suggest you start with the hard things first, list.js. it's mostly easy to learn and get used to but it will slowly feel like a chore, so get that out of the way first.
+Once you're done with that, head over to index.html, where you will edit the site name, embed, discord link, and records link. there are some other things you might change depending on your list,
+but that's up to you to figure out.
+that's all for now, and see you in release 1.5.1!
+---
+# Shitty List Staff Team
+---
+## Owners:
+- Cyns
+- Fulva
+- Sydney
+---
+## List Admins:
+- GabeLucario
+- Kobra
+- V1nnyy
+- Acidius
+---
+## List Moderators:
+- Goodyman
+- AcropolisBoy
+---
+## List Helpers:
+- Tril
+- dots
+---
+## Server Administrators:
+- Lupe
+- Nezzi
+- Electro
+- krissi
+- Yeezo
+- Magma
+- Bluestone
+---
+## Repo Maintainers:
+- Electro
+- Cyns
